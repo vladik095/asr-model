6fef698cfddd691397e763c4a50a63c1fd0dbc5b
Vladislav <ilusevicv@gmail.com>
2026-07-22 16:12:11 +0300

    fix Dataset

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -1,4 +1,5 @@
 import json
+import os
 import string
 
 import torch
@@ -7,67 +8,60 @@ from torch.utils.data import DataLoader, Dataset
 
 
 class BaseDataset(Dataset):
-    def __init__(self, 
-                 json_path, 
-                 text_encoder,
-                 transforms=None):
-        
-        alphabet = list(string.ascii_lowercase + " ")
+    def __init__(self, path_data_dir, text_encoder, transforms=None):
 
-        vocab = ["<blank>"] + alphabet
-        
-        self.char2idx = {
-        char: idx 
-        for idx, char in enumerate(vocab)}
-        
-        # print("VOCAB_SIZE", len(self.char2idx))
+        self.data_index = []
+
+        self._build_data_index(path_data_dir)
+
         self.text_encoder = text_encoder
         self.transforms = transforms
-        with open(json_path, 'r', encoding='utf-8') as f:
-            self.json_data = json.load(f) # obj: Dict(path_audio, text, audio_length)
- 
 
     def __getitem__(self, index):
-        data_dict = self.json_data[index]
+        data_dict = self.data_index[index]
 
         text = data_dict["text"]
-        audio_path = data_dict["path"]
-        audio_len = data_dict["audio_len"]
+        audio_path = data_dict["path_audio"]
 
         audio = self.get_audio(audio_path)
         spectrogram = self.get_spectrogram(audio)
         spectrogram = spectrogram.squeeze(0).transpose(0, 1)
 
-        text_encoded = torch.tensor(
-        self.text_encoder.encode(text),
-        dtype=torch.long
-        )
-        # print("TEXT: ", text)
-        # print(self._encode_text(text))
-        # print("SDSA")
-        # print(self.text_encoder.encode(text))
+        text_encoded = torch.tensor(self.text_encoder.encode(text), dtype=torch.long)
+
         data_obj = {
             "text": text,
             "text_encode": text_encoded,
             "spectrogram": spectrogram,
-            "audio_len": audio_len
         }
         return data_obj
 
-    
     def __len__(self):
-        return len(self.json_data)
-    
+        return len(self.data_index)
 
     def get_spectrogram(self, audio):
         mel_spectrogram = self.transforms(audio)
-        
+
         return mel_spectrogram
 
     def get_audio(self, path_audio):
-        waveform, sample_rate = torchaudio.load(path_audio)    
+        waveform, _ = torchaudio.load(path_audio)
         return waveform
 
-    def _encode_text(self, text):
-        tokens = [self.char2idx[char] for char in text if char in self.char2idx]
-        return tokens    
+    def _build_data_index(self, path_data_dir):
+        for dirname, _, filenames in os.walk(path_data_dir):
+            for filename in filenames:
+                full_path = os.path.join(dirname, filename)
+
+                if filename.endswith("trans.txt"):
+                    with open(full_path, "r", encoding="utf-8") as file:
+                        for line in file:
+                            line = line.strip()
+                            audio_id, text = line.split(maxsplit=1)
+
+                            audio_path = (
+                                os.path.dirname(full_path) + f"/{audio_id}.flac"
+                            )
+                            
+                            exmp = {"path_audio": audio_path, "text": text.lower()}
+                            self.data_index.append(exmp)
