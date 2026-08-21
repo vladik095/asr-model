b8a96b70c8d0d6d10916ca11cfb5d6958901fdab
Vladislav <ilusevicv@gmail.com>
2026-08-21 15:35:18 +0300

    ds

--- src/trainer/inferencer.py
+++ src/trainer/inferencer.py
@@ -13,86 +13,90 @@ class Inferencer:
         dataloader,
         text_encoder,
     ):
+
         self.model = model
         self.config = config
         self.device = device
         self.dataloader = dataloader
         self.text_encoder = text_encoder
+        
+        labels = [""] + list("abcdefghijklmnopqrstuvwxyz ")
 
         self.decoder = build_ctcdecoder(
-            labels=self.text_encoder.vocab,
-            kenlm_model_path=(
-                "/kaggle/input/datasets/"
-                "tuannguyenvananh/librispeech-4gram-language-model/"
-                "4-gram-librispeech.bin"
-            ),
+            labels=labels,               # ["<blank>", "a", ..., "z", " "]
+            kenlm_model_path="/kaggle/input/datasets/tuannguyenvananh/librispeech-4gram-language-model/4-gram-librispeech.bin"
         )
 
     def run_inference(self):
         self._from_pretraine()
 
         self.model.eval()
-
-        p_bar = tqdm(
-            self.dataloader,
-            desc="Infer",
-            total=len(self.dataloader),
-        )
-
-        wer_metric = WordErrorRate()
-        cer_metric = CharErrorRate()
-
+        p_bar = tqdm(self.dataloader, desc="Infer", total=len(self.dataloader))
         with torch.no_grad():
 
-            for batch in p_bar:
+            wer_metric = WordErrorRate()
+            cer_metric = CharErrorRate()
+
+            for _, batch in enumerate(p_bar):
 
                 spectrograms = batch["spectrograms"].to(self.device)
-                spectrogram_length = batch[
-                    "lens_spectrograms"
-                ].to(self.device)
-
-                output = self.model(
-                    spectrograms,
-                    spectrogram_length,
-                )
+                spectrogram_length = batch["lens_spectrograms"].to(self.device)
+                output = self.model(spectrograms, spectrogram_length)
 
                 log_probs = output["log_probs"]
                 log_probs_length = output["log_probs_length"]
 
-                # (B, T, vocab)
-                log_probs = log_probs.cpu()
-                log_probs_length = log_probs_length.cpu()
+                # preds = log_probs.argmax(dim=-1)
+                # preds = self.get_beam_LM_words(log_probs)
+                
+                # preds = preds.transpose(0, 1)
+                # pred_texts = self.get_pred_text(
+                #     self.text_encoder, preds, log_probs_length
+                # )
 
-                pred_texts = []
+                pred_texts = self.get_beam_LM_words(log_probs, log_probs_length)
 
-                for logits, length in zip(
-                    log_probs,
-                    log_probs_length,
-                ):
-                    logits = logits[:length]
-
-                    text = self.decoder.decode(
-                        logits.numpy(),
-                        beam_width=100,
-                        alpha=0.5,
-                        beta=1.5,
-                    )
-
-                    pred_texts.append(text)
-
-                wer_metric.update(
-                    pred_texts,
-                    batch["text"],
-                )
-
-                cer_metric.update(
-                    pred_texts,
-                    batch["text"],
-                )
+                wer_metric.update(pred_texts, batch["text"])
+                cer_metric.update(pred_texts, batch["text"])
 
                 print(
-                    "WER_val:",
+                    "WER_val ",
                     wer_metric.compute().item(),
-                    "CER_val:",
+                    "CER_val ",
                     cer_metric.compute().item(),
                 )
+
+    def _from_pretraine(self):
+
+        checkpoint = torch.load(
+            self.config.inferencer.from_pretrained, map_location=self.device
+        )
+        self.model.load_state_dict(checkpoint["model_state_dict"])
+        self.model.to(self.device) 
+
+    def get_pred_text(self, ctc_decoder, pred_tokens, spectrogram_length):
+        decoded = []
+
+        for pred, length in zip(pred_tokens, spectrogram_length):
+            decoded.append(ctc_decoder.ctc_decode(pred[:length]))
+
+        decoded_texts = [ctc_decoder.decode(tokens) for tokens in decoded]
+        decoded_texts = ["".join(chars) for chars in decoded_texts]
+        return decoded_texts
+
+    def get_beam_LM_words(self, log_probs, log_probs_length):
+        decoded_texts = []
+
+        for probs, length in zip(log_probs, log_probs_length):
+            probs = probs[:length]
+
+            text = self.decoder.decode(
+                probs.cpu().numpy(),
+                beam_width=100,
+                alpha=0.5,
+                beta=1.5,
+            )
+
+            decoded_texts.append(text)
+
+        return decoded_texts
